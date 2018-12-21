8118d5889fc5ac460694978b77a305cfb56a4f8f
richard baylis <veecher@gmail.com>
2018-12-21 14:15:56 -0800

    basic stuff

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,24 @@
+// * A GET Route to `/survey` which should display the survey page.
+// * A default, catch-all route that leads to `home.html` which displays the home page. 
+
+var path = require("path");
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
